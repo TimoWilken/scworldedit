ed1cc16d71efe23b9e5c9eb2dc3136ee4eb29e0c
Timo Wilken <timo.21.wilken@gmail.com>
2016-02-02 21:14:37 +0000

    Finish read_block_data function in blocks.py.
    blocks.py now has complete Block and ToolPower namedtuples and a complete read_block_data function. The script does nothing as of now.

--- blocks.py
+++ blocks.py
@@ -4,19 +4,34 @@ import xml.etree.ElementTree as ETree
 from collections import namedtuple
 
 
-Block = namedtuple('Block', 'id tool_longevity ')
+ToolPower = namedtuple('ToolPower', 'quarry shovel hack weapon longevity')
+Block = namedtuple('Block', 'id name power resilience blocks_fluid aimable '
+                            'light_attenuation light_emission max_stacking '
+                            'nutrition')
 
 
 def read_block_data(filename):
     root = ETree.parse(filename).getroot()
     for child in root:
-        assert child.tag == 'Block' and not len(child), \
-            'Root element should only contain <Block ... /> tags.'
+        if child.tag != 'Block' or len(child):
+            raise ValueError('Root element may only contain <Block /> tags.')
+        yield Block(id=int(child.get('BlockId')),
+                    name=child.get('Name'),
+                    power=ToolPower(*map(float, map(child.get, (
+                        'QuarryPower', 'ShovelPower', 'HackPower',
+                        'WeaponPower', 'AverageToolLongevity')))),
+                    resilience=float(child.get('DigResilience')),
+                    blocks_fluid=child.get('IsFluidBlocker') == 'True',
+                    aimable=child.get('IsAimable') == 'True',
+                    light_attenuation=int(child.get('LightAttenuation')),
+                    light_emission=int(child.get('EmittedLightAmount')),
+                    max_stacking=int(child.get('MaxStacking')),
+                    nutrition=float(child.get('NutritionalValue')))
 
 
 def main():
     """The script's main entry point."""
-    read_block_data('./blocks/BlocksData.xml')
+    blocks = read_block_data('BlocksData.xml')
 
 
 if __name__ == '__main__':
